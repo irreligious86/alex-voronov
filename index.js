e9df4628ad921449e64eb45e6640950c079382ca
fabysshk <fabysshk@gmail.com>
2021-02-02 15:24:44 +0200

    some fix

--- index.js
+++ index.js
@@ -31,25 +31,38 @@ const nFormatter = (num) => {
     return num;
 };
 app.get('/about',async (req, res) => {
-    const userName = 'irreligious86';
-    const data = await axios.get(`https://www.instagram.com/${userName}/?__a=1`);
-    const response = data.data;
-    const graphql = response.graphql;
-    let user = graphql.user;
-    user = {
-        ...user,
-        cover: user.profile_pic_url_hd,
+    let user = {
+        username:'irreligious86',
+        cover:'https://instagram.fiev19-1.fna.fbcdn.net/v/t51.2885-19/s320x320/137558605_121470649757844_2758978820028729241_n.jpg?_nc_ht=instagram.fiev19-1.fna.fbcdn.net&_nc_ohc=SiRtcGirFk8AX8Oc34F&tp=1&oh=9036ed74c92911c8d412c8d1c9171a8a&oe=60419DCC'
     }
-    let posts = user.edge_owner_to_timeline_media.edges;
-    posts = posts.map(post=>({
-        count:nFormatter(post.node.edge_media_to_comment.count),
-        likes:nFormatter(post.node.edge_liked_by.count),
-        cover: post.node.display_url
-    }));
+    let posts = [];
+    let errors = {}
+    try {
+        const userName = 'irreligious86';
+        const data = await axios.get(`https://www.instagram.com/${userName}/?__a=1`);
+        const response = data.data;
+        const graphql = response.graphql;
+        user = graphql.user;
+        user = {
+            ...user,
+            cover: user.profile_pic_url_hd,
+        }
+        posts = user.edge_owner_to_timeline_media.edges;
+        posts = posts.map(post=>({
+            count:nFormatter(post.node.edge_media_to_comment.count),
+            likes:nFormatter(post.node.edge_liked_by.count),
+            cover: post.node.display_url
+        }));
+    }catch (err){
+        console.log(err)
+        errors = err;
+    }
+
     res.render('about',{
         ...links('about'),
         user,
-        posts
+        posts,
+        errors:JSON.stringify(errors, null, 2)
     });
 })
 app.get('/portfolio', (req, res) => {
